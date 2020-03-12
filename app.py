2fb28e06838f6e8ad2739ee29d4a09e7e70fed03
ulou23 <ula.lukierska@gmail.com>
2020-03-12 21:53:45 +0100

    updated url

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import uuid
 
 # configuration
 DEBUG = True
@@ -16,29 +16,38 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 URLS = [
     {
-        'urlinput': 'On the Road',
+        'id':uuid.uuid4().hex,
+        'urlinput': 'http://bootstrap.com',
 
     },
     {
-        'urlinput': 'Harry Potter and the Philosopher\'s Stone',
+        'id':uuid.uuid4().hex,
+        'urlinput': 'https://koty.org',
 
     },
     {
-        'urlinput': 'Green Eggs and Ham',
-
-
+        'id':uuid.uuid4().hex,
+        'urlinput': 'https://onet.pl',
     }
 ]
 
+def remove_url(url_id):
+    for u in URLS:
+        if u['id'] == url_id:
+            URLS.remove(u)
+            return True
+    return False
 
-
+#####REST API HTTP METHODS
 
 @app.route('/urls', methods=['GET', 'POST'])
 def all_urls():
     response_object = {'status': 'success'}
     if request.method == 'POST':
         post_data = request.get_json()
-        URLS.append({ 'urlinput': post_data.get('urlinput'),
+        URLS.append({
+            'id': uuid.uuid4().hex,
+            'urlinput': post_data.get('urlinput'),
         })
         response_object['message'] = 'Url added!'
     else:
@@ -46,7 +55,18 @@ def all_urls():
     return jsonify(response_object)
 
 
-
+@app.route('/urls/<url_id>', methods=['PUT'])
+def single_url(url_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_url(url_id)
+        URLS.append({
+            'id': uuid.uuid4().hex,
+            'urlinput': post_data.get('urlinput')
+        })
+        response_object['message'] = 'URL updated!'
+    return jsonify(response_object)
 
 
 
