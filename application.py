54da8a05f36a51dd1f42f64ed54a0bf2abbc564b
ulou23 <ula.lukierska@gmail.com>
2020-03-22 18:10:14 +0100

    postgres baza send uri

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 # instantiate the app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:Freedom23@localhost:5432/vueflask"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://pswolsyqshnyxt:7087154f143fbb70a3c7f0c5e34ebb13deb5c5abc2d37adef4acd61cc7a5c360@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/dfssnvstarnqsc"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 from models import URLS, URLschema
@@ -67,29 +67,7 @@ def create_tables():
     db.create_all()
 
 
-# def remove_url(url_id):
-#    for u in URLS:
-#        if u['id'] == url_id:
-#            URLS.remove(u)
-#            return URLS  #wczesniej bylo tu True i nie dzialalo
-#    return False
 
-
-#####REST API HTTP METHODS
-
-# @app.route('/urls', methods=['GET', 'POST'])
-# def all_urls():
-#    response_object = {'status': 'success'}
-#    if request.method == 'POST':
-#        post_data = request.get_json()
-#        URLS.append({                        #url_schema=URLschema()
-#            'id': uuid.uuid4().hex,              # url,error =url_schema.load(data)
-#            'urlinput': post_data.get('urlinput'),                 #  result= url_schema(url.create()).data
-#        })
-#        response_object['message'] = 'Url added!'
-#    else:
-#        response_object['urls'] = URLS
-#    return jsonify(response_object)
 
 @app.route('/urls', methods=['GET'])
 def all_urls():
@@ -109,21 +87,7 @@ def CREATE():
     return make_response(jsonify({"url": result}))
 
 
-# @app.route('/urls/<url_id>', methods=['PUT', 'DELETE'])
-# def single_url(url_id):
-#    response_object = {'status': 'success'}
-#    if request.method == 'PUT':
-#        post_data = request.get_json()
-#        remove_url(url_id)
-#        URLS.append({
-#            'id': uuid.uuid4().hex,
-#            'urlinput': post_data.get('urlinput')
-#        })
-#        response_object['message'] = 'URL updated!'
-#    if request.method == 'DELETE':
-#        remove_url(url_id)
-#        response_object['message'] = 'URL removed!'
-#    return jsonify(response_object)
+
 
 @app.route('/urls/<url_id>', methods=['PUT', 'DELETE'])
 def single_url(url_id):
